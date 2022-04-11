2d978937c8e67513dfeec1cfec294bc53c601efa
root <root@iPhone>
2022-04-11 19:56:12 -0400

    mm-active

--- src/app/apps/shop/ecom-customers/ecom-customers.component.ts
+++ src/app/apps/shop/ecom-customers/ecom-customers.component.ts
@@ -13,23 +13,23 @@ export class EcomCustomersComponent implements OnInit {
   }
     
   
-  checkUncheckAll(event) {
-     var checkboxes = document.getElementsByTagName('input');
-     if (event.target.checked) {
-         for (var i = 0; i < checkboxes.length; i++) {
-             if (checkboxes[i].type == 'checkbox') {
-                 checkboxes[i].checked = true;
-             }
-         }
-     } else {
-         for (var i = 0; i < checkboxes.length; i++) {
-             // console.log(i)
-             if (checkboxes[i].type == 'checkbox') {
-                 checkboxes[i].checked = false;
-             }
-         }
-     }
- }
+ //  checkUncheckAll(event) {
+ //     var checkboxes = document.getElementsByTagName('input');
+ //     if (event.target.checked) {
+ //         for (var i = 0; i < checkboxes.length; i++) {
+ //             if (checkboxes[i].type == 'checkbox') {
+ //                 checkboxes[i].checked = true;
+ //             }
+ //         }
+ //     } else {
+ //         for (var i = 0; i < checkboxes.length; i++) {
+ //             // console.log(i)
+ //             if (checkboxes[i].type == 'checkbox') {
+ //                 checkboxes[i].checked = false;
+ //             }
+ //         }
+ //     }
+ // }
   
   
   
