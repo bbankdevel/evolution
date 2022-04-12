ae8c8a80fd7436e1ae3804786212c1df2040d3ac
root <root@iPhone>
2022-04-11 20:00:01 -0400

    bbev

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('kripton app is running!');
+    expect(await page.getTitleText()).toEqual('bbev app is running!');
   });
 
   afterEach(async () => {
